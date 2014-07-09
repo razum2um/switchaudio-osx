02da08b1a6147b48b97b76fa7c33259a8096c7b7
Vlad Bokov <razum2um@mail.ru>
2014-07-09 21:31:46 +0700

    no debug build

--- make.sh
+++ make.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # debug
 # gcc -g -o SwitchAudioSource main.c audio_switch.c -I. -framework CoreAudio
-gcc -g -o SwitchAudioSource main.c audio_switch.c -I. -framework CoreAudio
+gcc -o SwitchAudioSource main.c audio_switch.c -I. -framework CoreAudio
